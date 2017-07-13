3706bf73418473d0087de97a3961aed6bc4618d1
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-07-13 09:21:05 -0400

    addedid to create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+yomi = User.create({name: "Yomi"})
+Note.create({user_id: yomi.id, title:"Titlle Title Title", body: "STUFF STUFF STUFF"})
